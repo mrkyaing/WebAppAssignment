a4d4f275cacb576985635f5f0eeccba0df12f73e
Mg Kyiang <mr.kyaing7@gmail.com>
2021-01-21 23:40:45 +0630

    comsume rest api with restTemplate with basic Authentication

--- src/main/java/com/prodev/spring5/webapp/Spring5webappApplication.java
+++ src/main/java/com/prodev/spring5/webapp/Spring5webappApplication.java
@@ -11,46 +11,26 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class Spring5webappApplication {
-   static   String baseUrl = "http://localhost:8080/api/v1/sms/send/otp?customerPhone=";
+   static   String baseUrl = "http://localhost:8080/api/v1/sms/send/otp";
     public static void main(String[] args) throws URISyntaxException {
         SpringApplication.run(Spring5webappApplication.class, args);
        sendOtp("959256275319");
     }
 
-    private static HttpHeaders getHeaders ()
-    {
+    private static void sendOtp(String phone) {
+        String restUrl = baseUrl+"?customerPhone="+ phone;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = getHeaders();
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(restUrl,HttpMethod.GET, httpEntity, String.class);
+        System.out.println(responseEntity.getHeaders());
+    }
+    private static HttpHeaders getHeaders () {
         String adminuserCredentials = "dcheckadmin:dcheckadm!n";
-        String encodedCredentials =
-                new String(Base64.encodeBase64(adminuserCredentials.getBytes()));
-
+        String encodedCredentials = new String(Base64.encodeBase64(adminuserCredentials.getBytes()));
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Authorization", "Basic " + encodedCredentials);
         httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         return httpHeaders;
-    }
-
-    private static void sendOtp(String phone)
-    {
-
-
-        String restUrl = baseUrl+ phone;
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders httpHeaders = getHeaders();
-
-        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
-
-        ResponseEntity<String> responseEntity = restTemplate.exchange(restUrl,
-                HttpMethod.GET, httpEntity, String.class);
-
-        if(responseEntity.hasBody()) {
-            System.out.println(responseEntity.getBody());
-        }
-        else
-        {
-            System.out.println("User not found");
-        }
-
     }
 }
