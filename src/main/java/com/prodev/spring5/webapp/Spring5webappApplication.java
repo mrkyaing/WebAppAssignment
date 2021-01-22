0a6dcbbdd2ebf840b90616c1da9ea0eb538bcc5e
Mg Kyiang <mr.kyaing7@gmail.com>
2021-01-22 16:01:44 +0630

    sendOtp and verifyOtp with RestTemplate and Basic Auth

--- src/main/java/com/prodev/spring5/webapp/Spring5webappApplication.java
+++ src/main/java/com/prodev/spring5/webapp/Spring5webappApplication.java
@@ -11,35 +11,48 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class Spring5webappApplication {
-   static   String send_opt_Url = "http://localhost:8080/api/v1/sms/send/otp";
+   static String send_opt_Url = "http://localhost:8080/api/v1/sms/send/otp";
    static String verify_otp_url="http://localhost:8080/api/v1/sms/verify/otp";
    static RestTemplate restTemplate = new RestTemplate();
-    public static void main(String[] args) throws URISyntaxException {
+
+   public static void main(String[] args) throws URISyntaxException {
         SpringApplication.run(Spring5webappApplication.class, args);
-       //sendOtp("959256275319");
-       verifyOtp("959256275319",776718);
+       try {
+          String status=sendOtp("9592562753199");
+           System.out.println(status);
+       }catch (Exception e){
+           System.out.println(e.getMessage());
+       }
+
+       try {
+               String status=verifyOtp("959256275319",153149);
+               System.out.println("register success with otp");
+       }catch (Exception e){
+           System.out.println(e.getMessage());
+       }
+
     }
 
-    private static void verifyOtp(String phone, int otp) {
+    private static String verifyOtp(String phone, int otp) {
         verify_otp_url+="?customerPhone="+ phone;
         verify_otp_url+="&inputOtp="+otp;
 
         HttpHeaders httpHeaders = getHeaders();
         HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
         ResponseEntity<String> responseEntity = restTemplate.exchange(verify_otp_url,HttpMethod.GET, httpEntity, String.class);
-        System.out.println(responseEntity.getBody());
+       return  responseEntity.getBody();
     }
 
-    private static void sendOtp(String phone) {
+    private static  String  sendOtp(String phone) {
         send_opt_Url+="?customerPhone="+ phone;
-        HttpHeaders httpHeaders = getHeaders();
+        HttpHeaders httpHeaders = getHeaders();//get the HttpHeaders with user Credential of API EndPoint
         HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
         ResponseEntity<String> responseEntity = restTemplate.exchange(send_opt_Url,HttpMethod.GET, httpEntity, String.class);
-        System.out.println(responseEntity.getHeaders());
+       return responseEntity.getBody();
     }
     private static HttpHeaders getHeaders () {
-        String adminuserCredentials = "dcheckadmin:dcheckadm!n";
-        String encodedCredentials = new String(Base64.encodeBase64(adminuserCredentials.getBytes()));
+        String credentials = "dcheckadmin:dcheckadm!n";
+        String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Authorization", "Basic " + encodedCredentials);
         httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
